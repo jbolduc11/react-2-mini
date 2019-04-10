18cee270b5f7c46f56f3511e4e1b779bce0382d8
Jared <jaredbolduc@yahoo.com>
2019-04-09 22:20:23 -0600

    final update

--- src/App.js
+++ src/App.js
@@ -8,54 +8,54 @@ import FamilyChanger from './components/FamilyChanger';
 import TextContainer from './components/TextContainer';
 
 class App extends Component {
-   constructor() {
-     super();
-     this.state = {
-       fontColor: 'black',
-       fontSize: 12,
-       fontFamily: 'monospace',
-       allowEdit: 'true'
-     };
+  constructor() {
+    super();
+    this.state = {
+      fontColor: 'black',
+      fontSize: 12,
+      fontFamily: 'monospace',
+      allowEdit: 'true'
+    };
 
-     this.updateColor = this.updateColor.bind(this);
-     this.updateSize = this.updateSize.bind(this);
-     this.updateFamily = this.updateFamily.bind(this);
-     this.updateEditStatus = this.updateEditStatus.bind(this);
-   }
-  
+    this.updateColor = this.updateColor.bind( this );
+    this.updateSize = this.updateSize.bind( this );
+    this.updateFamily = this.updateFamily.bind( this );
+    this.updateEditStatus = this.updateEditStatus.bind( this );
+  }
+
   updateColor(val) {
-   this.setState({ fontColor: val });
+    this.setState({ fontColor: val });
   }
 
   updateSize(val) {
-    this.updateSize({ fontSize: val });
+    this.setState({ fontSize: val });
   }
 
   updateFamily(val) {
-    this.updateFamily({ fontFamily: val });
+    this.setState({ fontFamily: val });
   }
 
   updateEditStatus(val) {
-    this.updateEditStatus({ allowEdit: val });
+    this.setState({ allowEdit: val });
   }
 
   render() {
     return (
       <div>
         <div className="headerBar">
-          <EditToggle update={this.updateEditStatus} />
-          <ColorChanger update={this.updateColor} allowEdit={ this.state.allowEdit } />
+          <EditToggle update={ this.updateEditStatus } />
+          <ColorChanger update={ this.updateColor } allowEdit={ this.state.allowEdit } />
           <SizeChanger update={ this.updateSize } allowEdit={ this.state.allowEdit } />
-          <FamilyChanger update={ this.updateFamily} allowEdit={this.state.allowEdit } />
+          <FamilyChanger update={ this.updateFamily } allowEdit={ this.state.allowEdit } />
         </div>
         <div className="textArea">
-        <TextContainer 
+          <TextContainer 
             fontColor={ this.state.fontColor }
             fontSize={ this.state.fontSize }
             fontFamily={ this.state.fontFamily } />
         </div>
       </div>
-    );
+    )
   }
 }
 
